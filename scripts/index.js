89d01c6e82ac706d9daecbaed9d9bb1ced99b7f7
MoeHeinKhant005 <moeheink578@gmail.com>
2024-03-13 09:12:17 +0630

    Add generateHeroMovies func and some CSS changes
    Added generateHeroMovies function to generate movies in heroSection and added some CSS changes to heroSection

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,5 @@
 const apiKey = '531427db4c2df639e1c2babd568734c5';
+const imageBaseUrl = 'https://image.tmdb.org/t/p/w1280';
 
 // Fetch movies data
 async function fetchMovies(type){
@@ -6,3 +7,20 @@ async function fetchMovies(type){
     const data = await response.json();
     return data.results;
 }
+
+// Generate movies in hero section
+function generateHeroMovies(movieList){
+    const heroSec = document.querySelector('.hero-section-wrapper');
+    for(let i = 0; i < 5; i++){
+        heroSec.innerHTML += 
+        `<div class="hero-item" style="background-image: linear-gradient(to bottom, rgba(0,0,0,0.0), rgba(0,0,0,0.5)),url(${imageBaseUrl + movieList[i].poster_path});">
+            <span class="hero-item-name">${movieList[i].original_title}</span>
+            <div class="hero-item-trailer">
+                <i class="fa-solid fa-circle-play trailer-icon"></i>
+                <a href="https://www.youtube.com/results?search_query=${movieList[i].original_title}" class="trailer-text">Click to watch the trailer.</a>
+            </div>
+        </div>`;
+    }
+}
+
+fetchMovies('now_playing').then(data => generateHeroMovies(data))
